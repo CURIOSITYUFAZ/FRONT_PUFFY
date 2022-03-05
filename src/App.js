b25d40d92bce7c00ab2b66dcb6fdee5fab0dbf26
PufflyMan <ahmedovyusif8785@gmail.com>
2022-03-05 14:54:33 +0400

    color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div>
-      <header>
-        <p>
-         hi
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
